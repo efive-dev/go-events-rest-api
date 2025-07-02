c40b575e6ac4a4534e010fe007d2789e85463e2a
efive-dev <edoardo.quinto.dev@gmail.com>
2025-07-02 22:16:15 +0200

    Refactor: changed how routes are handled

---
+++ routes/routes.go
@@ -0,0 +1,10 @@
+// Package routes: establishes which endpoint to serve and how
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
+	server.POST("/events", createEvent)
+}
