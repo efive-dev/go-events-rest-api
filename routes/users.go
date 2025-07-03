423d348db393b39f19b369265073a18eb176ae6c
efive-dev <edoardo.quinto.dev@gmail.com>
2025-07-03 14:44:14 +0200

    Started working on users function (signup)

---
+++ routes/users.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"main/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signUp(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
